fc7a5d36f2e3d6eb598c6b7d841bae56dbe29d64
Admin <p.styla@gmail.com>
2019-06-08 16:41:07 +0200

    prawie :) działająca wersja

--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,46 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-        public class Main {
+public class Main {
 
-            public static void main(String[] args) {
-                Scanner in = new Scanner(System.in);
-                System.out.println("\n");
-                System.out.println("Hej, zagramy w kółko - krzyżyk?");
-                System.out.println("Tak? To podaj swoje imię:");
-                String imie1 = in.next();
-                System.out.println("Super " + imie1 + "!");
-                System.out.println();
-                System.out.println("A teraz Ty może podasz swoje imie: ");
-                String imie2 = in.next();
-                System.out.println("O extra " + imie2 + "!");
-                System.out.println("No to zaczynamy!");
-                System.out.println("\n");
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("\n");
+        System.out.println("Hej, zagramy w kółko - krzyżyk?");
+        System.out.println("Tak? To podaj swoje imię:");
+        String imie1 = in.next();
+        System.out.println("Super " + imie1 + "!");
+        System.out.println();
+        System.out.println("A teraz Ty może podasz swoje imie: ");
+        String imie2 = in.next();
+        System.out.println("O extra " + imie2 + "!");
+        System.out.println("No to zaczynamy!");
+        System.out.println("\n");
 
-                System.out.println("Na początek wylosujmy kto będzie zaczynał i stawiał krzyżyki...");
-                System.out.println("Zaczynamy? Jeśli tak - napisz TAK");
-                String tak = in.next();
+        System.out.println("Na początek wylosujmy kto będzie zaczynał i stawiał krzyżyki...");
+        System.out.println("Zaczynamy? Jeśli tak - napisz TAK");
+        String tak = in.next();
 
 
-                Random losowanie = new Random();
-                int a = losowanie.nextInt(2);
-                if (a == 1) {
-                    System.out.println("Krzyżyki stawia " + imie1 + " a kółka " + imie2);
-                } else {
-                    System.out.println("Krzyżyki stawia " + imie2 + " a kółka " + imie1);
-                }
-                System.out.println("\n");
+        Random losowanie = new Random();
+        int a = losowanie.nextInt(2);
+        if (a == 1) {
+            System.out.println("Krzyżyki stawia " + imie1 + " a kółka " + imie2);
+        } else {
+            System.out.println("Krzyżyki stawia " + imie2 + " a kółka " + imie1);
+        }
+        System.out.println("\n");
 
-                System.out.println("Oto nasza plansza - jeżeli chcesz postawić swój znak na " +
-                        "danym polu to wpisz poniżej numer tego pola:");
-                System.out.println("1  2  3");
-                System.out.println("4  5  6");
-                System.out.println("7  8  9");
-                if (a == 1) {
-                    System.out.println("Zaczyna " + imie1 + ". Gdzie stawiasz swój znak?");
-                } else {
-                    System.out.println("Zaczyna " + imie2 + ". Gdzie stawiasz swój znak?");
-                }
+        System.out.println("Oto nasza plansza - jeżeli chcesz postawić swój znak na " +
+                "danym polu to wpisz poniżej numer tego pola:");
+        System.out.println("1  2  3");
+        System.out.println("4  5  6");
+        System.out.println("7  8  9");
+        if (a == 1) {
+            System.out.println("Zaczyna " + imie1 + ". Gdzie stawiasz swój znak?");
+        } else {
+            System.out.println("Zaczyna " + imie2 + ". Gdzie stawiasz swój znak?");
+        }
 
 
         char[][] tab = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
@@ -164,7 +164,7 @@ import java.util.Scanner;
         System.out.print(tab[2][2]);
 
         System.out.println("\n");
-        System.out.println("Twój trzeci ruch "+ imie2);
+        System.out.println("Twój trzeci ruch " + imie2);
         char g = in.next().charAt(0);
         for (int i = 0; i < tab.length; i++) {
             for (int j = 0; j < tab[0].length; j++) {
@@ -190,7 +190,7 @@ import java.util.Scanner;
 
         int[] A = {b - 48, d - 48, f - 48};
         Arrays.sort(A);
-        int[] B = {c-48,e-48,g-48};
+        int[] B = {c - 48, e - 48, g - 48};
         Arrays.sort(B);
         int[] alfa1 = {1, 2, 3};
         int[] alfa2 = {4, 5, 6};
@@ -202,8 +202,6 @@ import java.util.Scanner;
         int[] alfa8 = {3, 5, 7};
 
         System.out.println("\n");
-
-
 
 
         if (Arrays.toString(A).equals(Arrays.toString(alfa1))) {
@@ -241,8 +239,7 @@ import java.util.Scanner;
             System.out.println(imie2 + " GRATULACJE WYGRAŁEŚ!");
         } else if (Arrays.toString(B).equals(Arrays.toString(alfa8))) {
             System.out.println(imie2 + " GRATULACJE WYGRAŁEŚ!");
-        }
-        else{
+        } else {
             System.out.println("remis!");
         }
     }
