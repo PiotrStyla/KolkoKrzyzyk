47137fd856cd9c92b8d9a85cd9df4912816d8aaf
Admin <p.styla@gmail.com>
2019-06-08 16:46:00 +0200

    prawie :) działająca wersja choinki i kilka innych ćwiczeń

--- src/OdwrocenieTextu.java
+++ src/OdwrocenieTextu.java
@@ -6,7 +6,7 @@ public class OdwrocenieTextu {
         System.out.println("Wprowadz text: ");
         char[] letters = scanner.nextLine().toCharArray();
         System.out.println("Odwrocony tekst:");
-        for (int i = letters.length-1; i >=0;i--) {
+        for (int i = letters.length - 1; i >= 0; i--) {
             System.out.print(letters[i]);
         }
         System.out.println("\n");
--- src/Unikalne123.java
+++ src/Unikalne123.java
@@ -4,12 +4,12 @@
 public class Unikalne123 {
     public static void main(String[] args) {
         int unikaty = 0;
-        for (int i = 1; i <=4 ; i++) {
-            for (int j = 1; j <=4 ; j++) {
-                for (int k = 1; k <=4 ; k++) {
-                    if(k !=i && k!=j && i!=j){
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                for (int k = 1; k <= 4; k++) {
+                    if (k != i && k != j && i != j) {
                         unikaty++;
-                        System.out.println(i+" "+j+" "+k);
+                        System.out.println(i + " " + j + " " + k);
                     }
                 }
             }
